fcb9c669068de5f4cddd8f3de46a3810d6e20ce4
ellian-aragao <ellian.aragao@gmail.com>
2019-11-14 01:26:15 -0300

    add funcionalidade para display de 7 seguimentos

--- casaAcessivel.ino
+++ casaAcessivel.ino
@@ -1,8 +1,17 @@
 // iniciação das entradas dos sensores
 const int interruptor = 0;
-const int ledVermelho = 3;
-const int ledAzul = 4;
 const int sensorGas = A0;
+
+// iniciação das saídas
+const int ledVermelho = 1;
+const int ledAzul = 2;
+const int A =  7;
+const int B =  8;
+const int C =  9;
+const int D = 10;
+const int E = 11;
+const int F = 12;
+const int G = 13;
 
 // definição de ativação do sensor de gás
 const int ativacao = 1023 / 4;
@@ -16,6 +25,49 @@ void piscaLed(unsigned int time, int led)
     delay(time);
 }
 
+void display(int x)
+{
+    switch (x)
+    {
+        case 0:
+            pinWrite(A, HIGH);
+            pinWrite(B, HIGH);
+            pinWrite(C, HIGH);
+            pinWrite(D, HIGH);
+            pinWrite(E, HIGH);
+            pinWrite(F, HIGH);
+            break;
+        case 1:
+
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+            
+            break;
+        case 5:
+            
+            break;
+        case 6:
+            
+            break;
+        case 7:
+            
+            break;
+        case 8:
+            
+            break;
+        case 9:
+            
+            break;
+    }
+}
+
 void setup()
 {
     // configura a velocidade de leitura
@@ -25,10 +77,17 @@ void setup()
     pinMode(sensorGas, INPUT);
     pinMode(ledVermelho, OUTPUT);
     pinMode(interruptor, INPUT);
+    pinMode(A, OUTPUT);
+    pinMode(B, OUTPUT);
+    pinMode(C, OUTPUT);
+    pinMode(D, OUTPUT);
+    pinMode(E, OUTPUT);
+    pinMode(F, OUTPUT);
 }
 
 void loop()
 {
+    display(0);
     // verifica se a leitura do interruptor é verdadeira
     if (digitalRead(interruptor))
     {
@@ -41,8 +100,8 @@ void loop()
         // faz o led piscar pela variável segundos
         for (int i = 0; i < seg; i++) piscaLed(time, ledAzul);
     }
-
-    Serial.println(analogRead(A0));
+    int analog = analogRead(A0)
+    //Serial.println(analog);
 
     // enquanto o sensor tiver passado do limite de ativacao ficará no loop
     while (analogRead(A0) >= ativacao)
